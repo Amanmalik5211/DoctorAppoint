36757d0bb6b82bb9c1550bc948a088bd84823726
Amanmalik5211 <133687588+Amanmalik5211@users.noreply.github.com>
2024-09-29 00:11:17 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://doctor-backend-0hqs.onrender.com';
+export const BASE_URL = 'https://doctor-backend-0hqs.onrender.com/api/v1';
 export const token = localStorage.getItem('token');
